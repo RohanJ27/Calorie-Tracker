499fdcaf3e43a60adfa061c4535a44580eadbb12
Rohan Jadhav <itsrohanj@gmail.com>
2024-11-14 20:37:08 -0800

    Fixed .env file issues on my end and connected Mongo to files using dotenv

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ mongoose
     process.exit(1);
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });

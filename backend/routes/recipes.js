08c068489eadc13be40eaccfef22067ff7bcc398
Aidan Luu <luunhantuan@gmail.com>
2024-12-01 01:46:53 -0800

    Added User to recipes.js to fix no user found bug when searching

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const auth = require('../middleware/auth');
+const User = require('../models/User');
 const dotenv = require('dotenv');
 
 dotenv.config();
